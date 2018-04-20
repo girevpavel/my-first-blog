dcb3f162f4e01d0b4330e3ff86b99e81028347a7
girevpavel <girevpavel@gmail.com>
2018-04-20 10:47:18 +0500

    added post detail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,4 +10,6 @@ def post_list(request):
 	
 	return render(request, 'blog/post_list.html', {'posts': posts})
 	
-	
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
